7707804cbb3afed9bb41e548361baa08617e8532
Anar Mamedov <anar26271@gmail.com>
2022-08-15 23:40:54 +0300

    as

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Menü
-$(".toggleMenu").on('click', function() {
+$(".toggleMenu").on('click', function () {
   $("#mainMenu").toggleClass('open');
 });
 // menü son
@@ -40,12 +40,12 @@ setTimeout(() => {
   share.classList.remove("hover");
 }, 3000);
 
-$('.card-of-product .share_button').on('click', function() {
+$('.card-of-product .share_button').on('click', function () {
   $('.card-of-product .sm-wrapper:visible').toggle('show')
   $(this).parents('.card-of-product').find('.sm-wrapper:hidden').toggle('show')
 })
 
-$(document).on("click", function(event) {
+$(document).on("click", function (event) {
   if ($(event.target).closest('.card-of-product .share_button').length === 0) {
     $('.card-of-product .sm-wrapper:visible').toggle('show')
   }
@@ -53,12 +53,12 @@ $(document).on("click", function(event) {
 
 // yeşil kartların paylaş butonu için
 
-$('.card-of-product1 .share_button').on('click', function() {
+$('.card-of-product1 .share_button').on('click', function () {
   $('.card-of-product1 .sm-wrapper:visible').toggle('show')
   $(this).parents('.card-of-product1').find('.sm-wrapper:hidden').toggle('show')
 })
 
-$(document).on("click", function(event) {
+$(document).on("click", function (event) {
   if ($(event.target).closest('.card-of-product1 .share_button').length === 0) {
     $('.card-of-product1 .sm-wrapper:visible').toggle('show')
   }
@@ -66,12 +66,12 @@ $(document).on("click", function(event) {
 
 // ürün detay sayfası paylaş butonu için
 
-$('.share_and_favorite_button .share_button').on('click', function() {
+$('.share_and_favorite_button .share_button').on('click', function () {
   $('.share_and_favorite_button .sm-wrapper:visible').toggle('show')
   $(this).parents('.share_and_favorite_button').find('.sm-wrapper:hidden').toggle('show')
 })
 
-$(document).on("click", function(event) {
+$(document).on("click", function (event) {
   if ($(event.target).closest('.share_and_favorite_button .share_button').length === 0) {
     $('.share_and_favorite_button .sm-wrapper:visible').toggle('show')
   }
@@ -80,20 +80,20 @@ $(document).on("click", function(event) {
 
 // Urun puanlama
 const coffeeCupContainers = $('.coffee_cup')
-coffeeCupContainers.each(function() {
+coffeeCupContainers.each(function () {
   const selectedCups = $(this).attr('data-selected')
   if (selectedCups === undefined) return false
 
   const cups = $(this).children()
 
-  cups.each(function(idx) {
+  cups.each(function (idx) {
     if (idx === +selectedCups) return false
     const cup = $(this)
     cup.css('opacity', 1)
   })
 })
 
-$(document).on('click', '.coffee_cup img', function() {
+$(document).on('click', '.coffee_cup img', function () {
   const currImage = $(this)
   const imagesContainer = currImage.parents('.coffee_cup')
   const isCurrImageSelected = +currImage.css('opacity') === 1
@@ -110,7 +110,7 @@ $(document).on('click', '.coffee_cup img', function() {
 
 $('.tabgroup > div').hide();
 $('.tabgroup > div:first-of-type').show();
-$('.tabs a').click(function(e) {
+$('.tabs a').click(function (e) {
   e.preventDefault();
   var $this = $(this),
     tabgroup = '#' + $this.parents('.tabs').data('tabgroup'),
@@ -125,12 +125,12 @@ $('.tabs a').click(function(e) {
 
 // ------------- üye olurken cinsiyet seçimi butonları için class ismi ekleme ve kaldırma ------------
 
-$('#female').click(function() {
+$('#female').click(function () {
   if ($(this).addClass("active")) {
     $('#man').removeClass('active');
   }
 });
-$('#man').click(function() {
+$('#man').click(function () {
   if ($(this).addClass("active")) {
     $('#female').removeClass('active');
   }
@@ -138,7 +138,7 @@ $('#man').click(function() {
 
 // ----------- ürünleri satın alırken adet kaç adet seçimi için ürün sayaç ---------------
 
-$('.button-minus').click(function() {
+$('.button-minus').click(function () {
   const el = $(this)
   const input = el.parents('.prosuct_count').find('input:text')
   const minusBtn = el.parents('.prosuct_count').find('.button-minus')
@@ -158,7 +158,7 @@ $('.button-minus').click(function() {
   }
 });
 
-$('.button-plus').click(function() {
+$('.button-plus').click(function () {
   const el = $(this)
   const input = el.parents('.prosuct_count').find('input:text')
   const minusBtn = el.parents('.prosuct_count').find('.button-minus')
@@ -181,17 +181,17 @@ $('.button-plus').click(function() {
 
 // --------- Ürün detay sayfası gramaj seçimi için class ismi ekleme ve silme ----------
 
-$('.250gr').click(function() {
+$('.250gr').click(function () {
   if ($(this).addClass("active")) {
     $('.350gr, .500gr').removeClass('active');
   }
 });
-$('.350gr').click(function() {
+$('.350gr').click(function () {
   if ($(this).addClass("active")) {
     $('.250gr, .500gr').removeClass('active');
   }
 });
-$('.500gr').click(function() {
+$('.500gr').click(function () {
   if ($(this).addClass("active")) {
     $('.250gr, .350gr').removeClass('active');
   }
@@ -204,7 +204,7 @@ $('.500gr').click(function() {
 var mybutton = document.getElementById("myBtn");
 
 // Sayfanın üst kısmından 20 piksel aşağı kaydırdığında düğmeyi göster
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction()
 };
 
@@ -225,9 +225,9 @@ function topFunction() {
 
 // sepetten ürün silme tuşu (removebutton clası eklenen butonlar dummy classlı parentini siliyor)
 
-$(".removebutton").on("click", function() {
+$(".removebutton").on("click", function () {
 
-  $(this).parents('.dummy').fadeOut(500, function() {
+  $(this).parents('.dummy').fadeOut(500, function () {
     // silerken fadeout effecti için üstteki parents(dummy) yazıldığında effecti dummy class lı dive veriyor
     $(this).parents('.dummy').remove();
   });
@@ -248,7 +248,7 @@ $(".number_of_product_cart").text(" " + cartnumm + " ");
 // ____________________ copy to clipboard özelliği için indirim kuponlarının kodlarını kopyalamak için _____________________
 
 
-$(".copy").on('click', function() {
+$(".copy").on('click', function () {
   var text = $(this).parents(".discount_coupon1").find(".coupon_code").text()
   console.log(text)
   var $temp = $("<input>");
@@ -258,3 +258,6 @@ $(".copy").on('click', function() {
   $temp.remove();
 
 });
+
+
+$('#nihad').customSelect();
